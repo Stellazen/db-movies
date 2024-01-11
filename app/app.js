0916abd1226c57f96013882dbe88520c634e78e2
unknown <stellazen.xd@gmail.com>
2024-01-10 21:28:46 -0300

    feat: configuração tsConfig

--- app/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function App() {
-    return (console.log("teste aqui"));
-}
-exports.default = App;
---
+++ dist/app.js
@@ -0,0 +1,4 @@
+function App() {
+    return (console.log("teste aqui"));
+}
+export default App;
