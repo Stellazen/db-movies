8d92d132412830e4fee3559f6a374ff4ed1c180c
unknown <stellazen.xd@gmail.com>
2024-01-10 21:01:07 -0300

    feat: primeiras instalações

---
+++ app/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function App() {
+    return (console.log("teste aqui"));
+}
+exports.default = App;
---
+++ app/app.tsx
@@ -0,0 +1,9 @@
+ import React = require("react");
+
+ function App(){
+    return(
+        console.log("teste aqui")
+    )
+ }
+
+ export default App;
